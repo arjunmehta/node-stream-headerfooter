8a4ee55c10168e121d2cdacc6486f333ec797a59
Arjun Mehta <arjunmeht@gmail.com>
2015-02-11 20:45:01 -0400

    Added an arbitrary test to get going.

--- test/test.js
+++ test/test.js
@@ -10,6 +10,23 @@ exports['Exported Properly'] = function(test) {
     test.done();
 };
 
+exports['New Out'] = function(test) {
+    test.expect(2);
+
+    var add_headerfooter = new headerfooter.Out();
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    test.equal(typeof add_headerfooter.header, 'object');
+    test.equal(typeof add_headerfooter.footer, 'object');
+
+    test.done();
+};
 
 exports['tearDown'] = function(done) {
     done();
