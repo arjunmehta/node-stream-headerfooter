52542f2a44713f96040a309f04a9fe5a9736b666
Arjun Mehta <arjunmeht@gmail.com>
2015-02-17 16:34:56 -0400

    Adding faster brake test.

--- test/test.js
+++ test/test.js
@@ -247,6 +247,51 @@ exports['Test Braked'] = function(test) {
     passthroughA.end("...Ending");
 };
 
+
+exports['Test Faster Braked'] = function(test) {
+    test.expect(6);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    var read_headerfooter = new headerfooter.In();
+
+    read_headerfooter.on('header', function(header) {
+    	console.log("Header", header);
+        test.equal(typeof header, 'object');
+        test.equal(header.name, 'streamA');
+        test.equal(header.metadata[0], 23);
+        test.equal(header.metadata[3], 222);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+    	console.log("Footer", footer);
+        test.equal(typeof footer, 'object');
+        test.equal(footer.exit_code, 2);
+        test.done();
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(brake(5000)).pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
 exports['tearDown'] = function(done) {
     done();
 };
